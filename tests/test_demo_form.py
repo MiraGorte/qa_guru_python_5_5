20499799e52b85577aa52c6c2ade9bf6c36dfe98
MiraGorte <sushchinskaya.m@gmail.com>
2023-04-21 21:34:33 +0300

    create folder 'tests'

--- tests/test_demo_form.py
+++ tests/test_demo_form.py
@@ -1,6 +1,37 @@
-from selene import browser
-
+from selene import browser, have
+import os
 
 def test_demo_form():
-    browser.open("https://demoqa.com/automation-practice-form")
+    browser.open('https://demoqa.com/automation-practice-form')
+
+    browser.element('#firstName').type('Mira')
+    browser.element('#lastName').type('Gorte')
+    browser.element('#userEmail').type('Mira_Gorte@gmail.com')
+    browser.element('[for = gender-radio-2]').bouble_click()
+    browser.element('#userNamber').type('89009002020')
+
+    browser.element('#dateOfBirthInput').click()
+    browser.element('react-datepicker__year-select').click()
+    browser.element('[value="2000"]').click()
+    browser.element('react-datepicker__month-select').click()
+    browser.element('[value="8"]').click()
+    browser.element('[aria-label="Choose Friday, September 8th, 2000"]').click()
+
+    browser.element('#subjectsInput').type('Chemistry').press_enter()
+    browser.element('[for="hobbies-checkbox-2"]').click()
+    browser.element('#uploadPicture').send_keys(os.getcwd() + '/cat_internet.jpg')
+
+    browser.element('#currentAddress').type('Moscow city')
+    browser.element('#react-select-3-input').type('NCR').press_enter()
+    browser.element('#react-select-4-input').type('Delhi').press_enter()
+
+    browser.element('#submit').press_enter()
+
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.all('tbody tr').should(have.exact_texts(
+      'Student name Mira Gorte' 'Student Email Mira_Gorte@gmail.com', 'Gender Female',
+        'Mobile 89009002020', 'Date of Birth 8 September,2000', 'Subjects Chemistry', 'Hobbies Reading',
+        'Picture cat_internet.jpg', 'Address Moscow', 'State and City NCR Delhi'))
+
+
 
